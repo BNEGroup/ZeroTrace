ee88cfcb7fe05489a0d8c7adfd1b540e22265b6f
BNEGroup <bernhard@nagy.pm>
2025-05-21 00:58:33 +0200

    Fix: App korrigiert

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -4,3 +4,9 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs) {
   return twMerge(clsx(inputs));
 }
+export const kostenarten = [
+  "Wartung", "Reparatur", "Versicherung", "Steuer", "Zubehör",
+  "Tuning", "HU/AU", "Reifen", "Pflege", "Sonstiges"
+];
+
+export const waehrungen = ["EUR", "HUF", "USD"];
