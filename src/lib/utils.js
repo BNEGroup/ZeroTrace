8ffe09d5d86c7fcead51de82f7c850b5f05a87dc
BNEGroup <bernhard@nagy.pm>
2025-05-21 11:16:10 +0200

    Fix: App korrigiert

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -1,12 +1,22 @@
-import { clsx } from "clsx";
-import { twMerge } from "tailwind-merge"
+// src/lib/utils.js
 
-export function cn(...inputs) {
-  return twMerge(clsx(inputs));
-}
+export const kraftstoffArten = [
+  "BioDiesel", "Diesel", "GTL Diesel", "HVO100", "Premium Diesel",
+  "Pflanzenöl", "Super 95", "Super E10", "Super 98", "Super Plus 103",
+  "LPG", "LNG", "Wasserstoff"
+];
+
 export const kostenarten = [
   "Wartung", "Reparatur", "Versicherung", "Steuer", "Zubehör",
   "Tuning", "HU/AU", "Reifen", "Pflege", "Sonstiges"
 ];
 
+export const wiederholungen = [
+  "Keine", "Jährlich", "Alle 2 Jahre", "Alle 3 Jahre", "Alle 6 Monate"
+];
+
 export const waehrungen = ["EUR", "HUF", "USD"];
+
+export const fahrweisen = ["Stadt", "Landstraße", "Autobahn", "Gemischt"];
+export const streckenarten = ["Kurzstrecke", "Langstrecke", "Gemischt"];
+export const reifenarten = ["Sommerreifen", "Winterreifen", "Ganzjahresreifen"];
